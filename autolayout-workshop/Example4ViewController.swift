3233d4458a649dcc089766683bd5e0ae7397efa5
Michał Kasprzyk <michal.kasprzyk@schibsted.pl>
2016-07-19 21:29:38 +0200

    Example 4 - start

---
+++ autolayout-workshop/Example4ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Example4ViewController.swift
+//  autolayout-workshop
+//
+//  Created by Michał Kasprzyk on 7/19/16.
+//  Copyright © 2016 Michał Kasprzyk. All rights reserved.
+//
+
+import UIKit
+
+class Example4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
