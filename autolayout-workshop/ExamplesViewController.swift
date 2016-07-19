cc95a9e95d6b043e92d2bdcec4d367482d0e1f3b
Michał Kasprzyk <michal.kasprzyk@schibsted.pl>
2016-07-19 19:51:57 +0200

    Basic project setup

---
+++ autolayout-workshop/ExamplesViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ExamplesViewController.swift
+//  autolayout-workshop
+//
+//  Created by Michał Kasprzyk on 7/19/16.
+//  Copyright © 2016 Michał Kasprzyk. All rights reserved.
+//
+
+import UIKit
+
+struct Example {
+    let name: String
+    let description: String
+}
+
+class ExamplesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var examples: [Example] = [
+    ]
+}
+
+extension ExamplesViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return examples.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExampleCell", for: indexPath)
+        cell.textLabel?.text = examples[indexPath.row].name
+        cell.detailTextLabel?.text = examples[indexPath.row].description
+        return cell
+    }
+}
+
+extension ExamplesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let segueIdentifier: String = "showExample\(indexPath.row)"
+        performSegue(withIdentifier: segueIdentifier, sender: nil)
+    }
+}
