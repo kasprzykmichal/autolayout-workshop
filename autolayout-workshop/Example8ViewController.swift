a70de8f73641a0d0bcc8f1c47aaf2596650c890f
Michał Kasprzyk <michal.kasprzyk@schibsted.pl>
2016-07-20 11:07:04 +0200

    Example 8 - start

---
+++ autolayout-workshop/Example8ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Example8ViewController.swift
+//  autolayout-workshop
+//
+//  Created by Michał Kasprzyk on 7/20/16.
+//  Copyright © 2016 Michał Kasprzyk. All rights reserved.
+//
+
+import UIKit
+
+class Example8ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
