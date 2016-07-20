31f0b891eb4e5b169a4164b44ef0f8d8da82d112
Michał Kasprzyk <michal.kasprzyk@schibsted.pl>
2016-07-20 11:11:34 +0200

    Example 8 - end

--- autolayout-workshop/Example8ViewController.swift
+++ autolayout-workshop/Example8ViewController.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class Example8ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var contentViewHeightConstraint: NSLayoutConstraint!
+    
+    private var changed: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGestureRecognizer()
+    }
+    
+    private func setupGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(Example8ViewController.tap(sender:)))
+        scrollView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func tap(sender: UITapGestureRecognizer) {
+        changed = !changed
+        contentViewHeightConstraint.constant = changed ? 1500 : 150
     }
 }
